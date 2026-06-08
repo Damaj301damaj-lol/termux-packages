940f68999275ca62374e23d069691396a5230f7a
Mohamad Damaj <mohamad301Damaj@duck.com>
2026-06-08 19:46:48 +0000

    bump(main/ack-grep): 3.10.0

--- packages/ack-grep/build.sh
+++ packages/ack-grep/build.sh
@@ -2,13 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://beyondgrep.com/
 TERMUX_PKG_DESCRIPTION="Tool like grep optimized for programmers"
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=(3.9.0
-					1.18)
+TERMUX_PKG_VERSION=(3.10.0 
+1.18)
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=("https://github.com/beyondgrep/ack3/archive/refs/tags/v${TERMUX_PKG_VERSION[0]}.tar.gz"
-					"https://github.com/petdance/file-next/archive/refs/tags/${TERMUX_PKG_VERSION[1]}.tar.gz")
-TERMUX_PKG_SHA256=(b2233b9f30b7099db2bad9d6b7e7f7e1003a73ce24353b92b8e904d5b44fedf7
-					e3c56f0bdcb3251a2e22b9ffc3c7cbf99b1433a45fc8a07f8e2e66174bc3963f)
+"https://github.com/petdance/file-next/archive/refs/tags/${TERMUX_PKG_VERSION[1]}.tar.gz")
+TERMUX_PKG_SHA256=(ef1df66e8bd5cee004473231423cf2033450b8f2955f2020628361ff80a5675e
+e3c56f0bdcb3251a2e22b9ffc3c7cbf99b1433a45fc8a07f8e2e66174bc3963f)
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 TERMUX_PKG_DEPENDS="perl"
